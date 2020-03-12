2cc1242abaa8a67c0e5e908f1a0679a4ebb02f46
William Batista <ninja25538@tutanota.de>
2020-03-12 18:59:32 +0000

    Fixed a bug where the time and price didn't change
    So it turns out, every time the thread was called,
    the price of the currencies and the current time didn't change,
    since those were both called in the very begging of the log.py program
    I just moved them down to the logData function

--- log.py
+++ log.py
@@ -17,53 +17,53 @@ def stopLogging():
 
 def logPriceData():
 
-currencies = {
-        "time" : time.time(),
-        "BAT" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('BAT', 1)
-        },
-        "ETH" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('ETH', 1)
-        },
-        "DASH" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('DASH', 1)
-        },
-        "DOGE" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('DOGE', 1)
-        },
-        "STEEM" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('STEEM', 1)
-        },    
-        "XMR" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('XMR', 1)
-        },
-        "ETN" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('ETN', 1)
-        },
-        "SBD" : {
-            "price" : cmc.convert('SBD', 1)
-        },
-        "GRC" : {
-            #Remember to actually change the price, I'm just keeping it like this to save credits
-            #"price" : cmc.convert('XMR', 1),
-            "price" : cmc.convert('GRC', 1)
-        }
+    currencies = {
+            "time" : time.time(),
+            "BAT" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('BAT', 1)
+            },
+            "ETH" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('ETH', 1)
+            },
+            "DASH" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('DASH', 1)
+            },
+            "DOGE" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('DOGE', 1)
+            },
+            "STEEM" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('STEEM', 1)
+            },    
+            "XMR" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('XMR', 1)
+            },
+            "ETN" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('ETN', 1)
+            },
+            "SBD" : {
+                "price" : cmc.convert('SBD', 1)
+            },
+            "GRC" : {
+                #Remember to actually change the price, I'm just keeping it like this to save credits
+                #"price" : cmc.convert('XMR', 1),
+                "price" : cmc.convert('GRC', 1)
+            }
 
-}
+    }
 
 
     global kill
@@ -75,7 +75,7 @@ currencies = {
             historicalData.write(json.dumps(currencies) + "\n")
             historicalData.close()
             logging.debug(json.dumps(currencies))
-            threading.Timer(300, logPriceData).start()
+            threading.Timer(10, logPriceData).start()
         else:
             print("Reached maximum number of lines")
     else:
